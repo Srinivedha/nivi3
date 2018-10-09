ae3c35eff3b1dfae97fdf4d4bb64102dc319cd3f
Srinivedha <srinivedhaperiyasamy@gmail.com>
2018-10-09 14:07:04 +0530

    Create mid.java

---
+++ mid.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+class mid
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int arr[]=new int[a];
+        for(int i=0;i<a;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        Arrays.sort(arr);
+        if(a%2==0)
+        {
+         int k=a/2;
+         System.out.print(arr[k]);
+        }
+        else
+        {
+            int k=(a/2)+1;
+            System.out.print(arr[k]);
+        }
+    }
+}
