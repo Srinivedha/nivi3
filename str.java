baab5eeb92754b3964d25c23e2a9909480d4a57e
Srinivedha <srinivedhaperiyasamy@gmail.com>
2018-10-09 14:35:34 +0530

    Create str.java

---
+++ str.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+class str
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        int len=a.length();
+        int count=0;
+        String b="1234567890";
+        for(int i=0;i<len;i++)
+        {
+            if(b.contains(a.charAt(i)+""))
+            {
+                count++;
+            }
+        }
+        if(count==len)
+        {
+            System.out.print("Yes");
+        }
+        else
+        {
+            System.out.print("No");
+        }
+    }
+}
